67a47faacf9b0917be391bcf8e105b481418eec8
David Plitnizki <davidplitnizki@Davids-MBP.lan>
2026-03-28 21:40:35 +0300

    init app

---
+++ Pinpals/PinpalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinpalsApp.swift
+//  Pinpals
+//
+//  Created by David Plitnizki on 28/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PinpalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
